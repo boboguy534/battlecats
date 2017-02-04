c841962ddd4788d7d9c38b306dcf82b7847d515a
boboguy534 <roxy.tablets@gmail.com>
2017-02-04 11:46:54 -0600

    two player!

--- js/main.js
+++ js/main.js
@@ -1,17 +1,45 @@
+
+Cat = function(x, y, id) {
+	this.elem = $("#"+id);
+	this.elem.css({"top":x+"px", "left":y+"px"});
+}
+
+Cat.prototype.move = function(dx, dy) {
+	this.elem.animate({"top":"+="+dy+"px", "left":"+="+dx+"px"}, 10);
+}
+
+
 $("#game").ready(function() {
+	var cat1 = new Cat(50, 100, "cat-1");
+	var cat2 = new Cat(350, 100, "cat-2");
 	$(this).on("keydown", function(event) {
+		var speed = 30;
 		switch(event.key) {
 			case "w":
-				$("#cat").animate({"top":"-=10px"});
+				cat1.move(0,-speed);
 				break;
 			case "s":
-				$("#cat").animate({"top":"+=10px"});
+				cat1.move(0,speed);
 				break;
 			case "a":
-				$("#cat").animate({"left":"-=10px"});
+				cat1.move(-speed,0);
 				break;
 			case "d":
-				$("#cat").animate({"left":"+=10px"});
+				cat1.move(speed,0);
+				break;
+		}
+		switch(event.which) {
+			case 38:
+				cat2.move(0,-speed);
+				break;
+			case 40:
+				cat2.move(0,speed);
+				break;
+			case 37:
+				cat2.move(-speed,0);
+				break;
+			case 39:
+				cat2.move(speed,0);
 				break;
 		}
 	});
